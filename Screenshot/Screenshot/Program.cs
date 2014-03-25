04309ca0f098cc2aea681e13fcd2245b8ce08eb2
vulamicy <vulamicy@gmail.com>
2014-03-25 14:19:38 +0700

    tao moi project
    ok ngon roi day

---
+++ Screenshot/Screenshot/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Screenshot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = "Chup_" + DateTime.Now.ToString("ddMMyyyyhhmmtt");
+            try
+            {
+                Console.WriteLine("Chup man hinh, nhan OK");
+                Console.Read();
+                ScreenCapturer.CaptureAndSave(fileName);
+                Console.WriteLine("Da chup: C:\\temp\\" + fileName + ".png");
+                
+                logErr("Da chup: C:\\temp\\" + fileName + ".png");
+                Console.ReadKey(true);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Loi: " + ex.Message);
+            }            
+        }
+        static void logErr(string str)
+        {
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            string logPath = path + "\\Info-TC7.txt";
+            System.IO.StreamWriter file = new System.IO.StreamWriter(logPath, true);
+            file.WriteLine(str);
+            file.Close();
+            file.Dispose();
+        }
+    }
+}
